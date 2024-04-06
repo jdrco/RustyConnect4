431aebe9973fe01c653a1f0e14872b9d54295ce1
Antonio Martin-O <antonio2@ualberta.ca>
2024-04-06 17:52:54 -0600

    Added Rules Components

--- src/components/app.rs
+++ src/components/app.rs
@@ -1,12 +1,14 @@
-use crate::components::connect4board::Connect4board;
-use crate::components::tootandottoboard::TootAndOttoBoard;
+use crate::components::connect4board::{Connect4Board, Connect4Rules};
+use crate::components::tootandottoboard::{TootAndOttoBoard, TootAndOttoRules};
 use yew::prelude::*;
 
 #[function_component]
 pub fn App() -> Html {
     html! {
         <>
-            <Connect4board />
+            <Connect4Rules />
+            <Connect4Board />
+            <TootAndOttoRules />
             <TootAndOttoBoard />
         </>
     }
