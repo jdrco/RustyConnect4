facffeee2818ef16a9bf2a509dbe11798234f1a0
Antonio Martin-O <antonio2@ualberta.ca>
2024-04-06 20:31:50 -0600

    Visual update

--- src/components/burgermenu.rs
+++ src/components/burgermenu.rs
@@ -20,7 +20,7 @@ pub fn burger_menu(props: &Props) -> Html {
 
     html! {
         <div class="relative">
-            <button onclick={toggle_visibility} class="block text-gray-500 hover:text-black focus:outline-none">
+            <button onclick={toggle_visibility} class={format!("block {} hover:text-black focus:outline-none", if *is_hidden {"text-gray-500"} else {"text-black"})}>
                 <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M1 4.5C1 3.67157 1.67157 3 2.5 3H21.5C22.3284 3 23 3.67157 23 4.5C23 5.32843 22.3284 6 21.5 6H2.5C1.67157 6 1 5.32843 1 4.5ZM1 11.5C1 10.6716 1.67157 10 2.5 10H21.5C22.3284 10 23 10.6716 23 11.5C23 12.3284 22.3284 13 21.5 13H2.5C1.67157 13 1 12.3284 1 11.5ZM1 18.5C1 17.6716 1.67157 17 2.5 17H21.5C22.3284 17 23 17.6716 23 18.5C23 19.3284 22.3284 20 21.5 20H2.5C1.67157 20 1 19.3284 1 18.5Z"/>
                 </svg>
